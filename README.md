a9155a3dcc388d47e05a0ff7a022a2ba8aab106c
eliss <elis.staaf@proton.me>
2025-01-10 11:26:49 +0100

    Ready, set, go!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # fright-knight
-A li'l game I made. And no, it's not copyright infingement (I hope...).
+A li'l game I made. And no, it's not copyright infingement (I hope...). You're
+in reality playing ``Fright Knight DELUXE``, which I made when I remade the game 
+a few months ago.
---
+++ src/Scenes/menu_main.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+func _on_level_1_pressed():
+	get_tree().change_scene_to_file("res://Scenes/castle.tscn")
+
+
+func _on_level_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/escape.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://Scenes/snow.tscn")
+
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://Scenes/hideout.tscn")
+
+
+
+func _on_level_5_pressed():
+	get_tree().change_scene_to_file("res://Scenes/celestial.tscn")
+
+
+func _on_level_6_pressed():
+	get_tree().change_scene_to_file("res://Scenes/climb.tscn")
